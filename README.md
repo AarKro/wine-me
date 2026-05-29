b62b24ee8a0184ceef9c1be7a66f4ec8f03a1ae0
Aaron Kromer <kromer.aaron@gmail.com>
2026-05-29 13:57:47 +0200

    add proper build and deployment workflow

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Gesture-Controlled interactive image viewer.
 
 ## Demo
-https://aarkro.github.io/wine-me/g
+https://aarkro.github.io/wine-me/
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/wine-me/",
   server: {
     host: "localhost",
     port: 5173,
